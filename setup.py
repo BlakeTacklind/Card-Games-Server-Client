5a4f3a7596e93e3064359a1b9bea58b359b6f2d3
Blake Tacklind <blake@tacklind.com>
2015-09-01 01:42:51 -0700

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-play',
+	version='0.0.0',
+	author='Blake Tacklind',
+	author_email='blake@tacklind.com',
+	url='git@github.com:BlakeTacklind/python-play.git',
+	)
