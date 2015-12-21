981b211d2eb441d64d4ac102043e11344601cc3a
Blake Tacklind <blake@tacklind.com>
2015-12-21 00:06:47 -0800

    Simple web socket implementation

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='python-play',
 	version='0.0.0',
 	author='Blake Tacklind',
 	author_email='blake@tacklind.com',
 	url='git@github.com:BlakeTacklind/python-play.git',
+	install_requires=['autobahn']
 	)
