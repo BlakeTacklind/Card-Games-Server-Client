68d5326082158bb3beac71b377be9666dce2329f
Blake Tacklind <blake@tacklind.com>
2016-01-24 21:07:33 -0800

    Edited for Nginx html server. Doesn't work on windows

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: __dirname + "/dist",
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js',
   },
